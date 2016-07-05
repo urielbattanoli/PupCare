1ecd8f23227a23dac91edfd9ea99b8578535bd08
Luis Filipe Campani <luisfilipecampani@gmail.com>
2016-07-05 09:05:55 -0300

    getPetshopList - Search near PetShops, order them by distance.

--- pupcare_app/cloud/main.js
+++ pupcare_app/cloud/main.js
@@ -6,10 +6,17 @@ Parse.Cloud.define("getPetshopList", function(request, response) {
 
   var query = new Parse.Query("PetShop");
 
+  var geoPoint = request.params.currentLocation;
+
+  query.near("centerPoint", geoPoint);
+  query.limit(20);
   query.find({
       success: function(results) {
-        Console.log(results.length);
-        response.success(results);
+        var distances = [];
+        for (var i = 0; i < results.length; ++i){
+          distances.push(results[i].kilometersTo(geoPoint));
+        }
+        response.success(distances);
       },
       error: function() {
         response.error("PetShop lookup failed");
