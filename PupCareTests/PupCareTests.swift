817ec1418af8c928d2cad5b7c589a4c6f69ca58a
Anderson Gralha <anderson.gralha@gmail.com>
2016-07-11 15:23:32 -0300

    Added ParseManager

--- PupCareTests/PupCareTests.swift
+++ PupCareTests/PupCareTests.swift
@@ -7,12 +7,16 @@
 //
 
 import XCTest
+
 @testable import PupCare
 
 class PupCareTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        
+        ParseManager.InitParse()
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
