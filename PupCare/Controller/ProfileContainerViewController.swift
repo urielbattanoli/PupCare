65d246c144755de9ee0cf2ece2badf86ab5a316f
Luis Filipe Campani <luisfilipecampani@gmail.com>
2016-10-05 16:26:01 -0300

    W E G U C C I N O W B O Y Z

---
+++ PupCare/Controller/ProfileContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileContainerViewController.swift
+//  PupCare
+//
+//  Created by Luis Filipe Campani on 05/10/16.
+//  Copyright © 2016 PupCare. All rights reserved.
+//
+
+import UIKit
+
+class ProfileContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
